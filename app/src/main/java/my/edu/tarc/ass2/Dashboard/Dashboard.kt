a27d2430d858e6b3e8880212aef16912fd36ce05
WTHUW <75290505+error323dino@users.noreply.github.com>
2023-05-07 23:50:21 +0800

    hmm

--- app/src/main/java/my/edu/tarc/ass2/Dashboard/Dashboard.kt
+++ app/src/main/java/my/edu/tarc/ass2/Dashboard/Dashboard.kt
@@ -2,17 +2,35 @@ package my.edu.tarc.ass2.Dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import my.edu.tarc.ass2.R
-import my.edu.tarc.ass2.databinding.ActivityMainBinding
+import my.edu.tarc.ass2.databinding.ActivityDashboardBinding
 
+private lateinit var binding: ActivityDashboardBinding
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.hotlinePic.setOnClickListener(){
+            val fragment = HotlineFragment()
+            supportFragmentManager.beginTransaction()
+                .add(android.R.id.content, fragment)
+                .addToBackStack(null)
+                .commit()
+
+        }
+
+        binding.profilePic.setOnClickListener(){
+            val navController = Navigation.findNavController(view)
+            val action = FragmentDirections.actionFragmentToAnotherFragment()
+            navController.navigate(action)
+        }
+
+
+
+
 
     }
 }

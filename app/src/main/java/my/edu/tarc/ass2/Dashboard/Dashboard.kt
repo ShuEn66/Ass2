c77506135e3292e4086351da65be9e11f925983b
WTHUW <75290505+error323dino@users.noreply.github.com>
2023-05-07 17:59:21 +0800

    hmm

--- app/src/main/java/my/edu/tarc/ass2/Dashboard/Dashboard.kt
+++ app/src/main/java/my/edu/tarc/ass2/Dashboard/Dashboard.kt
@@ -3,10 +3,16 @@ package my.edu.tarc.ass2.Dashboard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import my.edu.tarc.ass2.R
+import my.edu.tarc.ass2.databinding.ActivityMainBinding
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
     }
 }

e4445c794520192e5bae573ead38058b592cccac
WTHUW <75290505+error323dino@users.noreply.github.com>
2023-05-10 12:43:01 +0800

    ok

---
+++ app/src/main/java/my/edu/tarc/ass2/Payment/Payment.kt
@@ -0,0 +1,12 @@
+package my.edu.tarc.ass2.Payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.edu.tarc.ass2.R
+
+class Payment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}

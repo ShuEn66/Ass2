37cf4e20221f6739526be5c27e5bedbf95f28157
WTHUW <75290505+error323dino@users.noreply.github.com>
2023-05-13 16:47:03 +0800

    hen fan

--- app/src/main/java/my/edu/tarc/ass2/RoomDatabase.kt
+++ app/src/main/java/my/edu/tarc/ass2/RoomDatabase.kt
@@ -7,8 +7,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-
-
 @Database(entities = [User::class, Payment::class, ElectricityAcc::class, Bill::class, Appliances::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun databaseDao(): databaseDao

1cd8a1bfaf521a7a1b050fbdc617ab63926c2674
cheeqian <changcq-wm21@student.tarc.edu.my>
2023-05-14 01:13:27 +0800

    apps

--- app/src/main/java/my/edu/tarc/ass2/AppSettings/AppSettingsFragment.kt
+++ app/src/main/java/my/edu/tarc/ass2/AppSettings/AppSettingsFragment.kt
@@ -10,17 +10,6 @@ import androidx.navigation.fragment.findNavController
 import my.edu.tarc.ass2.R
 import my.edu.tarc.ass2.databinding.FragmentAppSettingsBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AppSettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AppSettingsFragment : Fragment() {
 
     private var _binding: FragmentAppSettingsBinding? = null
@@ -47,23 +36,4 @@ class AppSettingsFragment : Fragment() {
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AppSettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AppSettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/my/edu/tarc/ass2/AppSettings/ChangLanguageFragment.kt
+++ app/src/main/java/my/edu/tarc/ass2/AppSettings/ChangLanguageFragment.kt
@@ -8,17 +8,6 @@ import android.view.ViewGroup
 import my.edu.tarc.ass2.R
 import my.edu.tarc.ass2.databinding.FragmentChangLanguageBinding
 import my.edu.tarc.ass2.databinding.FragmentRatingBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChangLanguageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChangLanguageFragment : Fragment() {
     private var _binding: FragmentChangLanguageBinding? = null
     private val binding get() = _binding!!
@@ -40,23 +29,9 @@ class ChangLanguageFragment : Fragment() {
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChangLanguageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChangLanguageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
+
 }

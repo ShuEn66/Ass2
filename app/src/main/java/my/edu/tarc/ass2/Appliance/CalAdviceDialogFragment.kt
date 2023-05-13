aeb105dae95f6c5eab371041552dfcb6a989b895
JX <sammiexuan@gmail.com>
2023-05-14 04:08:06 +0800

    UI update

---
+++ app/src/main/java/my/edu/tarc/ass2/Appliance/CalAdviceDialogFragment.kt
@@ -0,0 +1,25 @@
+package my.edu.tarc.ass2.Appliance
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.edu.tarc.ass2.R
+import my.edu.tarc.ass2.databinding.FragmentCalAdviceDialogBinding
+import my.edu.tarc.ass2.databinding.FragmentCalHelpDialogBinding
+
+
+class CalAdviceDialogFragment : Fragment() {
+    private var _binding: FragmentCalAdviceDialogBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCalAdviceDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}

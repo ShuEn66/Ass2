351332b75f4a04bd4692f202aa61108e9006002e
WTHUW <75290505+error323dino@users.noreply.github.com>
2023-05-13 22:57:16 +0800

    hmm

--- app/src/main/java/my/edu/tarc/ass2/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/ass2/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
        //go dashboard from main activity
-        //navController.navigate(R.id.dashboard)
+        navController.navigate(R.id.dashboard)
 
         navController.addOnDestinationChangedListener{
                 _,destination,_->

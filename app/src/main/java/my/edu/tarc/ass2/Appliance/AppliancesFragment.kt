6fba5a8f466750fe5dd885c244ae60ea61e22ea2
JX <sammiexuan@gmail.com>
2023-05-13 21:50:55 +0800

    appliances main page

---
+++ app/src/main/java/my/edu/tarc/ass2/Appliance/AppliancesFragment.kt
@@ -0,0 +1,13 @@
+package my.edu.tarc.ass2.Appliance
+
+import android.os.Bundle
+import my.edu.tarc.ass2.R
+import androidx.appcompat.app.AppCompatActivity
+
+class AppliancesFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_appliances)
+    }
+}

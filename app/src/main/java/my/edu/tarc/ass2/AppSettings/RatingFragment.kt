b6a46c528b89ae114d887fb234102231a8d01c47
cheeqian <changcq-wm21@student.tarc.edu.my>
2023-05-14 13:29:46 +0800

    app ok le

--- app/src/main/java/my/edu/tarc/ass2/AppSettings/RatingFragment.kt
+++ app/src/main/java/my/edu/tarc/ass2/AppSettings/RatingFragment.kt
@@ -7,20 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import my.edu.tarc.ass2.R
 import my.edu.tarc.ass2.databinding.FragmentDeveloperInfoBinding
 import my.edu.tarc.ass2.databinding.FragmentRatingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RatingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RatingFragment : Fragment() {
     private var _binding: FragmentRatingBinding? = null
     private val binding get() = _binding!!
@@ -46,31 +37,9 @@ class RatingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonSubmitRating.setOnClickListener(){
-            Toast.makeText(context,"Thanks for submitting!",Toast.LENGTH_LONG).show()
-        }
-
-        binding.imageButtonClose.setOnClickListener(){
-
+            Toast.makeText(context,R.string.thanks,Toast.LENGTH_LONG).show()
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RatingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RatingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
